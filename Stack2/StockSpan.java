d072a015b12735c11d5454bf2420f6ad1409e899
Gourav Giri <codinggourav@gmail.com>
2025-04-11 17:34:32 +0530

    New code

--- Stack2/StockSpan.java
+++ Stack2/StockSpan.java
@@ -26,7 +26,7 @@ public class StockSpan {
         }
 
      }
-     public static void main(String[] args) {
+     public static void main(String[] args) { 
         int arr[]={100,80,60,70,60,85,100};
         int span[]=new int[arr.length];
         stockspan(arr, span);
